1278b714e7af41a6a49a1762087d5a90be85d714
Aftab <aftabmulani001@gmail.com>
2023-01-07 23:27:18 +0530

    updated

--- src/test/java/Jala/java/selenium/magnus/Locators/assignment1.java
+++ src/test/java/Jala/java/selenium/magnus/Locators/assignment1.java
@@ -1,9 +1,10 @@
-package Jala.java.selenium.magnus;
+package Jala.java.selenium.magnus.Locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 public class assignment1 {
+
     WebDriver driver;
     @Test(priority = 0)
     public void launch() throws InterruptedException{
@@ -24,10 +25,26 @@ public class assignment1 {
     }
     @Test(priority = 2)
     public void autoscript() throws InterruptedException {
+        driver.findElement(By.xpath("//input[@id='UserName']")).sendKeys("training@jalaacademy.com"); // 1st Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("jobprogram"); // 2nd Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@id='btnLogin']")).click(); // 3rd Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[normalize-space()='Employee']")).click(); // 4th Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[normalize-space()='Search']")).click(); // 5th Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='Name']")).sendKeys("vanll"); // 6th Automation
+		Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@id='btnSearch']")).click(); // 7th Automation
+		Thread.sleep(1000);
 
     }
     @Test(priority = 3)
     public void destroy() throws InterruptedException {
-        
+        Thread.sleep(1000);
+		driver.quit();
     }
+
 }
